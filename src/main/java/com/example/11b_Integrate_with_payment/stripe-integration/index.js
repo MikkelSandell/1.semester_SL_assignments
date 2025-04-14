f52db3f7f5dcf6a8fe7ec691d5198602926b7cf2
Mikkel sandell <mikkelsandell@gmail.com>
2025-04-14 14:34:28 +0200

    made assegmetn 11b

---
+++ src/main/java/com/example/11b_Integrate_with_payment/stripe-integration/index.js
@@ -0,0 +1,41 @@
+import 'dotenv/config'; // loads env variables
+import express from 'express';  
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Stripe Integration Running');
+});
+
+app.post('/create-checkout-session', async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'Cool Item',
+            },
+            unit_amount: 2000,
+          },
+          quantity: 1,
+        },
+      ],
+      success_url: 'http://localhost:8080/success',
+      cancel_url: 'http://localhost:8080/cancel',
+    });
+
+    res.json({ url: session.url });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(8080, () => console.log('Server running on http://localhost:8080'));
