88a4264b60274ca82459c2b1693b4392121f7b22
Mikkel sandell <mikkelsandell@gmail.com>
2025-05-11 15:46:47 +0200

    made a small change to index.js

--- src/main/java/com/example/11a_Auth_integration/backend/index.js
+++ src/main/java/com/example/11a_Auth_integration/backend/index.js
@@ -43,7 +43,8 @@ app.get('/logout', (req, res) => {
     res.redirect('/');
   });
 });
+
 const PORT = process.env.PORT ?? 8080;
-app.listen(process.env.PORT, () => {
-  console.log(`Server started on http://localhost:${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
 });
